d32bfa6588afc6183edd3065f20949b559a23d42
Cole Fuerth <colefuerth@gmail.com>
2022-10-14 14:20:42 -0400

    fix flatten layer to actually flatten

--- layers/Flatten.py
+++ layers/Flatten.py
@@ -4,8 +4,8 @@ class Flatten:
     def __init__(self):
         return None
 
-    def forward(self, X):
-        return X.reshape(X.shape[0], -1)
+    def forward(self, X:np.ndarray) -> np.ndarray:
+        return X.reshape(-1)
 
     def backward(self, grad_y_pred, learning_rate):
         return None
