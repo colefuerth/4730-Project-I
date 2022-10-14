01b02bc47091b021c07d2ec227a51987a721c7a7
Cole <colefuerth@gmail.com>
2022-10-14 03:22:09 -0400

    improved model definition

--- model.py
+++ model.py
@@ -10,6 +10,7 @@ from itertools import product
 from layers.Conv2D import Conv2D
 from layers.Pooling import Pooling
 from layers.Dense import Dense
+from layers.Flatten import Flatten
 
 # %%
 (train_X, train_y), (test_X, test_y) = mnist.load_data()
@@ -19,45 +20,59 @@ train_X, test_X = train_X / 255.0, test_X / 255.0
 train_X, test_X = train_X[:10000], test_X[:1000]
 train_y, test_y = train_y[:10000], test_y[:1000]
 
+train_X = train_X.reshape(-1, 28, 28, 1)
+test_X = test_X.reshape(-1, 28, 28, 1)
+
 print('Train: X=%s, y=%s' % (train_X.shape, train_y.shape))
 print('Test: X=%s, y=%s' % (test_X.shape, test_y.shape))
 
 # %%
 # define the model
 
+model = []
+model.append(Conv2D(32, 2, 1, 1))
+model.append(Pooling(2, 2, 'max'))
+model.append(Flatten())
+dims = train_X[0]
+for layer in model:
+    dims = layer.forward(dims)
+dims = dims.shape[0]
+# print(dims)
+model.append(Dense(dims, 128))
+model.append(Dense(128, 10))
+
 model = [
-    Conv2D(32, 2, 1, 1),
-    Pooling(2, 2, 'max'),
+    #Conv2D(32, 2, 1, 1),
+    #Pooling(2, 2, 'max'),
+    Flatten(),
     Dense(32*14*14, 100),
     Dense(100, 10)
 ]
 
 # %%
+# define functions for training
 
-def predict(X):
+def predict(X) -> np.ndarray:
+    # forward pass on a single image
     for layer in model:
         X = layer.forward(X)
     return X
 
-def predict_batch(X):
-    with mp.Pool() as pool:
-        return pool.map(predict, X)
-
-def learn(model, grad_y_pred, lr):
-    for layer in reversed(model):
-        grad_y_pred = layer.backward(grad_y_pred, lr)
-
-def learn_batch(model, grad_y_pred, lr):
-    with mp.Pool() as pool:
-        pool.starmap(learn, zip(model, grad_y_pred, lr))
+def predict_batch(X) -> np.ndarray:
+    # forward pass on a batch of images
+    return np.array(list(map(predict, X)))
 
 def train(X, y, lr=0.01):
-    y_pred = predict(X)
+    # forward pass
+    y_pred = predict_batch(X)
+    # calculate loss
     loss = np.square(y_pred - y).sum()
     print('loss: %f' % loss)
 
+    # backward pass
     grad_y_pred = 2.0 * (y_pred - y)
-    learn_batch(model, grad_y_pred, lr)
+    for layer in reversed(model):
+        grad_y_pred = layer.backward(grad_y_pred, lr)
 
 
 # %%
